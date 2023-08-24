20a7393270b04d51b8570fbba3e0ddc88dc2b4fc
destineevaldivia <destvaldivia@gmail.com>
2023-08-24 08:27:20 -0700

    test

--- butterflies.js
+++ butterflies.js
@@ -6,3 +6,11 @@ const butterflies = [{
     "format": "Paperback"
 }]
 export default butterflies;
+
+const test = [{
+    "type": "0553120441",
+    "title": "Harry Potter and the Sorcerer's Stone",
+    "author": "J.K. Rowling",
+    "format": "Paperback"
+}]
+export default test;
