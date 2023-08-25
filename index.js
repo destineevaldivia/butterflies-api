f94a9b1e4b6dea57634032eb28e52202bc052c7a
destineevaldivia <destvaldivia@gmail.com>
2023-08-25 08:51:32 -0700

    fixed error in imports

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
-import 'dotenv/config';
 import butterflies from './butterflies.js';
 
-//create instance of express.js framework application and set my port to var in my env or 5001
+//create instance of express.js framework application and set my port to 5001
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = 5001;
 
 //The path.resolve() method resolves a sequence of paths into an absolute path
 const __dirname = path.resolve();
@@ -14,10 +13,10 @@ const __dirname = path.resolve();
 //configuring cors middleware, this enables the express server to respond to preflight requests
 app.use(cors())
 
-//create my routes 
+//create endpoints
 app.get("/", (req, res) => {
-    res.json("root");
-    //res.sendFile(path.join(__dirname, 'index.html'));
+    //res.json("root");
+    res.sendFile(path.join(__dirname, 'index.html'));
   });
 
 app.get("/butterflies", (req, res) => {
@@ -25,9 +24,9 @@ app.get("/butterflies", (req, res) => {
     //res.sendFile(path.join(__dirname, 'index.html'));
   });
 
-  app.get("/butterflies2", (req, res) => {
-    res.json("butterflies2");
-  });
+  //app.get("/butterflies2", (req, res) => {
+    //res.json("butterflies2");
+  //});
 
 
   app.listen(PORT, () => console.log(`Hola! Server running on Port http://localhost:${PORT}`));
